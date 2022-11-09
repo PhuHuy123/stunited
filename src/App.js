39b5cea0519284bb8f355d9067e7da08681b5818
hieunqpd04773 <hieunqpd04773@fpt.edu.vn>
2022-11-09 11:12:28 +0700

    Base Dev Branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./style/App.scss";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Stories/index.js
@@ -0,0 +1,12 @@
+import clsx from "clsx";
+import styles from "./Stories.module.css";
+
+function Stories() {
+  return (
+    <div className={clsx(styles.mt, "d-flex")}>
+      <h1 className={clsx(styles.head)}> Hwlll</h1>
+      <h2> OK</h2>
+    </div>
+  );
+}
+export default Stories;

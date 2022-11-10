386793299cc5fa626a6b1861d57f0ec93e4eaade
hieunqpd04773 <hieunqpd04773@fpt.edu.vn>
2022-11-10 16:48:18 +0700

    Push Slider and Stories
    Fix and Responsive Home Slider-Stories

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,14 @@
+import Stories from "../../components/Stories";
+import Sliders from "../../components/Sliders";
 import Header from "../../components/Header";
-function Home() {
-  return <><Header/></>;
+const Home = () => {
+  return (
+    <>
+      <Header/>
+      <Sliders />
+      <Stories />
+    </>
+  );
 }
 
 export default Home;

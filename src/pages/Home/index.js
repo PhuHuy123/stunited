29c64eb78d557bb52c00079f2977f458aa3af978
NgyenPHuy <nguyenphuhuy180920@gmail.com>
2022-11-11 11:41:51 +0700

    Finished: footer
    fix responsive footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,15 +2,17 @@ import Stories from "../../components/Stories";
 import Sliders from "../../components/Sliders";
 import Header from "../../components/Header";
 import Partners from "../../components/clients-partners/clients-partners";
+import Footer from "../../components/Footer";
 const Home = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <Sliders />
       <Stories />
       <Partners />
+      <Footer />
     </>
   );
-}
+};
 
 export default Home;

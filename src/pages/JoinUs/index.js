ba8e89d2b7eecdac53e906da89eaab6d492d4d62
unknown <anhcanh1007@gmail.com>
2022-11-11 16:00:55 +0700

    feat : update feature/home-joinus

--- src/pages/JoinUs/index.js
+++ src/pages/JoinUs/index.js
@@ -1,12 +1,14 @@
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import { JoinUS } from "../../components/ContentJoinUs";
+import Footer from "@app/components/Footer";
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
       <JoinUS />
+      <Footer />
     </>
   );
 };

aeb0ba52803063511543c2db6bd432c1baadcbd1
Hiếu Nguyễn <115602015+hieudevplus@users.noreply.github.com>
2022-11-11 09:40:10 +0700

    Push Slider and Stories (#2)
    Fix and Responsive Home Slider-Stories
    
    Co-authored-by: hieunqpd04773 <hieunqpd04773@fpt.edu.vn>

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -1,12 +1,42 @@
 import clsx from "clsx";
-import styles from "./Stories.module.css";
+import styles from "./Stories.module.scss";
+import { useEffect, useState } from "react";
+import { storiesApi } from "../../config/apiService";
+import StoryCards from "./StoryCards";
 
-function Stories() {
+const Stories = () => {
+  const [stories, setStories] = useState([]);
+  useEffect(() => {
+    getStories();
+  }, []);
+  const getStories = async () => {
+    const res = await storiesApi();
+    if (res && res.data && res.data.length > 0) {
+      let arr = [];
+      res.data.map((item) => {
+        if (item.status === 1) {
+          arr.push(item);
+        }
+        return "ok";
+      });
+      return setStories(arr);
+    } else {
+      console.log("Stories have no data");
+    }
+  };
   return (
-    <div className={clsx(styles.mt, "d-flex")}>
-      <h1 className={clsx(styles.head)}> Hwlll</h1>
-      <h2> OK</h2>
+    <div className={clsx(styles.stories, "container")}>
+      <div className="row">
+        <div className={clsx(styles.heading, "text-center")}>
+          <h2>Typical successful stories</h2>
+          <h3 className={clsx(styles.sub)}>
+            From ideas turned into carefully crafted pixels and problems crafted
+            into solutions.
+          </h3>
+        </div>
+        <StoryCards stories={stories} />
+      </div>
     </div>
   );
-}
+};
 export default Stories;
--- src/config/apiService.js
+++ src/config/apiService.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
-const getApiMenu=()=>{
-    return axios.get('https://6369b58f28cd16bba7227e74.mockapi.io/Menu')
-}
-export{
-    getApiMenu,
-}
+const getApiMenu = () => {
+  return axios.get("https://6369b58f28cd16bba7227e74.mockapi.io/Menu");
+};
+
+ const storiesApi = () => {
+  return axios.get(
+    "https://6369c37115219b84961e2a6c.mockapi.io/api/devplus/stories"
+  );
+};
+
+ const slidersApi = () => {
+  return axios.get(
+    "https://6369c37115219b84961e2a6c.mockapi.io/api/devplus/sliders"
+  );
+};
+
+export {
+  getApiMenu,
+  storiesApi,
+  slidersApi,
+};

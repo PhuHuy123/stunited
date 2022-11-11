454748aee3d90918280824434a4f7776011cb03d
hieunqpd04773 <hieunqpd04773@fpt.edu.vn>
2022-11-11 11:41:02 +0700

    branch confusion
    Create Page PortFolio and Banner
    
    feat: edit package json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./style/App.scss";
 import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import JoinUs from "./pages/JoinUs";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/join-us" element={<JoinUs />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Portfolio/index.js
@@ -0,0 +1,14 @@
+import Portfolio from "../../components/Stories/Portfolio";
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Portfolio />
+    </>
+  );
+};
+
+export default Home;

9fc74ef9bc020c4f4c65bdaa7e262cf4462fb8ed
NgyenPHuy <nguyenphuhuy180920@gmail.com>
2022-11-11 17:17:58 +0700

    Finished: content top ST

--- src/pages/Digital/index.js
+++ src/pages/Digital/index.js
@@ -1,10 +1,14 @@
 import Header from "../../components/Header";
 import Footer from "@app/components/Footer";
+import Banner from "../../components/Banner";
 import { MeetOurTeamUS } from "@app/components/MeetOurteam";
+import ContentTop from "@app/components/Info-digital";
 const Digital = () => {
   return (
     <>
       <Header />
+      <Banner />
+      <ContentTop/>
       <MeetOurTeamUS />
       <Footer />
     </>
